ba802193020589f33ff571d61001e58bb130e4ef
PetNamedEr <skogberg.t.peter@student.uta.figit config --global user.emailgit config --global user.email skogberg.t.peter@student.uta.figit config --global user.email skogberg.t.peter@student.uta.fi>
2018-03-13 18:49:36 +0200

    -

--- models/survey.py
+++ models/survey.py
@@ -17,36 +17,8 @@ class Survey:
         self.description = description_
 
     def __repr__(self):
-        return "<Question: {}>".format(self.name)
+        return "<Survey: {}>".format(self.id)
 
 class surveyForm(Form):
     id = IntegerField("ID")
     description = StringField("Description")
-
-@app.route('/addNewSurvey', methods=['GET', 'POST'])
-def addNewSurvey():
-    form = surveyForm(request.form)
-
-    if request.method == 'POST' and form.validate():
-        new_survey = db.prepare("INSERT INTO survey VALUES ($1, $2)")
-        with db.xact():
-        	new_survey(form.id.data, form.description.data)
-        
-        flash("Survey " + str(form.id.data) + " " +  \
-            form.description.data + " saved successfully!")
-        return redirect('/')
-
-    return render_template('new_survey.html', form=form)
-
-@app.route('/printSurveys', methods=['GET', 'POST'])
-def printSurveys():
-    surveys = []
-    form = surveyForm(request.form)
-
-    all_the_surveys = db.prepare("SELECT * FROM survey")
-    
-    with db.xact():
-      for survey in all_the_surveys():
-        surveys.append(survey[1])
-    
-    return render_template('surveys.html', surveys=surveys)
