9ab7f3e2178c636b72fc03fbcafb7d4cac68c75c
iks <inka.simola@protonmail.com>
2018-03-14 03:53:06 +0200

    Pass postgres_url as command line parameter

--- db_setup.py
+++ db_setup.py
@@ -1,3 +1,4 @@
+import sys
 from sqlalchemy import create_engine
 from sqlalchemy import Column, ForeignKey, Integer, String, Date
 from sqlalchemy.orm import relationship, sessionmaker
@@ -9,9 +10,17 @@ from models.survey import Survey
 from models.feedback import Feedback
 from models.answer import Answer
 
-# Integration with database
-postgres_url = 'postgresql+psycopg2://insi:somepasswd@localhost:5432/llb'
-engine = create_engine(postgres_url)
-Base.metadata.create_all(engine)
+if __name__ == '__main__':
 
+    if len(sys.argv) < 2:
+        print('Usage: python db_setup.py <postgres_url>')
+        print('Example postgres_url: postgresql+psycopg2://insi:somepasswd@localhost:5432/llb')
+        sys.exit()
 
+    else:
+        postgres_url = sys.argv[1]
+        try:
+            engine = create_engine(postgres_url)
+            Base.metadata.create_all(engine)
+        except:
+            'Cannot bind to database'
