094a26caf8e3e381f024449921f74e69454f627f
iks <inka.simola@protonmail.com>
2018-03-14 02:49:20 +0200

    Add SQLAlchemy-compatible models for survey, answer, feedback

--- models/answer.py
+++ models/answer.py
@@ -1,21 +1,43 @@
+from sqlalchemy import Column, ForeignKey, Integer, String, Date
+# from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
 import datetime
 
-class Answer:
+from config.helper import Base
+from models.feedback import Feedback
+# from models.question import Question # UPDATE!
+
+class Answer(Base):
     __tablename__ = "answers"
 
-    id_ = int()
-    value = str()
-    created_at = datetime.datetime
-    question_id = int()
-    feedback_id = int()
+    # Mappers
+    id_ = Column(Integer, primary_key=True)
+    value = Column(String)
+    created_at = Column(Date, nullable=False)
+    feedback_id = Column(Integer, ForeignKey('feedback.id_')) # UPDATE!
+    feedback_ = relationship("Feedback")
+    # feedback_ = relationship("Feedback", back_populates="feedback")
+    # question = relationship(Question)
+    # question_id = Column(Integer, ForeignKey('question.id_')) # UPDATE!
 
-    def __init__(self, id_, value_, question_id_, feedback_id_):
+    # def __init__(self, id_, value_, question_id_, feedback_id_): # UPDATE!
+    def __init__(self, id_, value_, feedback_id_):
         self.id_ = id_
         self.value =  value_
         self.created_at = datetime.datetime.now()
-        self.question_id = question_id_
         self.feedback_id = feedback_id_
+        # self.question_id = question_id_
 
     def __repr__(self):
-        return "<Id: {}, Created_at: '{}', Question_id: {}, Feedback_id {}, Value: '{}'>".format(self.id_, self.created_at, self.question_id, self.feedback_id, self.value)
+        # return "<Id: {}, Created_at: '{}', Question_id: {}, Feedback_id {}, Value: '{}'>".format(self.id_, self.created_at, self.question_id, self.feedback_id, self.value)
+        return "<Id: {}, Created_at: '{}', Feedback_id: {}, Value: '{}'>".format(self.id_, self.created_at, self.feedback_id, self.value) # UPDATE!
 
+    @property
+    def serialize(self):
+        return {
+            'id_' : self.id_,
+            'value' : self.value,
+            'created_at' : self.created_at.isoformat(),
+            # 'question_id' : self.question_id,
+            'feedback_id' : self.feedback_id
+        }
--- models/feedback.py
+++ models/feedback.py
@@ -1,10 +1,26 @@
-class Feedback:
+from sqlalchemy import Column, ForeignKey, Integer, String, Date
+# from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+
+from config.helper import Base
+# from models.answer import Answer
+
+class Feedback(Base):
     __tablename__ = "feedback"
 
-    id_ = int()
+    # Mappers
+    id_ = Column(Integer, primary_key=True)
+    # answers_ = relationship("Answer")
+    # answers = relationship("Answer", back_populates="answers")
 
     def __init__(self, id_):
         self.id_ = id_
 
     def __repr__(self):
         return "<Id: {}>".format(self.id_)
+
+    @property
+    def serialize(self):
+        return {
+            'id_' : self.id_
+        }
