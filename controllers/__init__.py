22ec57d410695a989da9dc4ff9ec823c8dde3de9
iks <inka.simola@protonmail.com>
2018-04-29 08:38:10 +0300

    Add feedback routes

--- controllers/__init__.py
+++ controllers/__init__.py
@@ -11,8 +11,9 @@ from .Questions import routes as question_routes
 from .QuestionChoices import routes as questionchoice_routes
 from .Surveys import routes as survey_routes
 from .Results import routes as results_routes
+from .Feedbacks import routes as feedback_routes
 
-routes = (question_routes + survey_routes + questionchoice_routes + results_routes)
+routes = (question_routes + survey_routes + questionchoice_routes + results_routes + feedback_routes)
 
 for r in routes:
     mod.add_url_rule(
