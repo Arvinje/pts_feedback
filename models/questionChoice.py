e9009e943850748673d4d4766e4c4382f8cda0ab
iks <inka.simola@protonmail.com>
2018-04-29 08:43:22 +0300

    Add autoincrement. Fix __init__ and __repr__

--- models/questionChoice.py
+++ models/questionChoice.py
@@ -15,49 +15,30 @@ from config.setup import engine
 tablename = "questionchoices"
 
 class QuestionChoice(Base):
-  __tablename__ = tablename
+    __tablename__ = tablename
 
-<<<<<<< HEAD
-	# Mappers
-	id_ = Column(Integer, primary_key=True, autoincrement=True)
-	title_ = Column(String)
-=======
-  # Mappers
-  id_ = Column(Integer, primary_key=True)
-  title_ = Column(String)
->>>>>>> b221f43bf967fdc0574c9c40c272976b2067c9be
+    # Mappers
+    id_ = Column(Integer, primary_key=True)
+    title_ = Column(String)
 
-  # Question choice is child of question
-  question_id_ = Column(Integer, ForeignKey('questions.id_'))
-  questions = relationship("Question", back_populates="questionchoices")
+    # Question choice is child of question
+    question_id_ = Column(Integer, ForeignKey('questions.id_'))
+    questions = relationship("Question", back_populates="questionchoices")
 
-  def __init__(self, title_, question_id_):
-    self.title_ = title_
-    self.question_id_ = question_id_
+    def __init__(self, title_, question_id_):
+        self.title_ = title_
+        self.question_id_ = question_id_
 
-<<<<<<< HEAD
-	def __repr__(self):
-		return "<id_: {}, title_: {}, question_id_: {}>".format(self.id_, self.title_, self.question_id_)
+    def __repr__(self):
+        return "<id_: {}, title_: {}, question_id_: {}>".format(self.id_, self.title_, self.question_id_)
 
-	@property
-	def serialize(self):
-		return {
-			'id_' : self.id_,
-			'title_' : self.title_,
-			'question_id_' : self.question_id_
-		}
-=======
-  def __repr__(self):
-    return "<Id: {}, Title: {}, Question_id: {}>".format(self.id_, self.title_, self.question_id_)
-
-  @property
-  def serialize(self):
-    return {
-      'id_' : self.id_,
-      'title_' : self.title_,
-      'question_id_' : self.question_id_
-    }
->>>>>>> b221f43bf967fdc0574c9c40c272976b2067c9be
+    @property
+    def serialize(self):
+        return {
+            'id_' : self.id_,
+            'title_' : self.title_,
+            'question_id_' : self.question_id_
+        }
 
 if not engine.has_table(tablename):
-	Base.metadata.create_all(engine)
+    Base.metadata.create_all(engine)
