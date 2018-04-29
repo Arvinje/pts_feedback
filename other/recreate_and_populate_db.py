7cdf4c88908dc81bc60edc369c4bb030294f5e19
iks <inka.simola@protonmail.com>
2018-04-29 08:45:12 +0300

    Add optional_, fix add to db

--- other/recreate_and_populate_db.py
+++ other/recreate_and_populate_db.py
@@ -38,12 +38,12 @@ q1_c2 = QuestionChoice(title_="I hated it", question_id_=1)
 q2 = Question(type_="Freeform", title_="Q2: Tell us how you feel", survey_id_=1, optional_=True)
 
 # Survey 1 Q3
-q3 = Question(type_='Stars', title_='Q3: Smoothness of ride', survey_id_=1)
+q3 = Question(type_='Stars', title_='Q3: Smoothness of ride', survey_id_=1, optional_=True)
 session.add(q3)
 session.commit()
 
 # Survey 2 Q1
-q3 = Question(type_='Multiple choice', title_='Q1: Smoothness of ride', survey_id_=2, optional_=False)
+q3 = Question(type_='Multiple choice', title_='Q1: Smoothness of ride', survey_id_=1, optional_=False)
 q3_c1 = QuestionChoice(title_="1", question_id_=3)
 q3_c2 = QuestionChoice(title_="2", question_id_=3)
 q3_c3 = QuestionChoice(title_="3", question_id_=3)
@@ -51,16 +51,16 @@ q3_c4 = QuestionChoice(title_="4", question_id_=3)
 q3_c5 = QuestionChoice(title_="5", question_id_=3)
 
 # Survey 1 Q2
-q4 = Question(type_='Smileys', title_="Q2: Air quality", survey_id_=2, optional_=True)
+q4 = Question(type_='Smileys', title_="Q2: Air quality", survey_id_=1, optional_=True)
 q4_c1 = QuestionChoice(title_="frown", question_id_=3)
 q4_c2 = QuestionChoice(title_="meh", question_id_=3)
 q4_c3 = QuestionChoice(title_="incredible", question_id_=3)
 
 # Feedbacks
-fb_1 = Feedback()
-fb_2 = Feedback()
-fb_3 = Feedback()
-fb_4 = Feedback()
+fb_1 = Feedback(survey_id_=1)
+fb_2 = Feedback(survey_id_=1)
+fb_3 = Feedback(survey_id_=1)
+fb_4 = Feedback(survey_id_=1)
 
 # Answers from first feedback (survey was 1)
 a_f1_q1 = Answer(value_=q1_c1.title_, feedback_id_=1, question_id_=1)
@@ -81,7 +81,7 @@ a_f4_q4 = Answer(value_=q4_c2.title_, feedback_id_=4, question_id_=4)
 
 # Add objects to database
 # for obj in [admin_1, s1, q1, q1_c1, q1_c2, q2, s2, q3, q3_c1, q3_c2, q3_c3, q3_c4, q3_c5, q4, q4_c1, q4_c2, q4_c3, fb_1, fb_2, fb_3, fb_4, a_f1_q1, a_f1_q2, a_f2_q1, a_f2_q2, a_f3_q3, a_f3_q4, a_f4_q3, a_f4_q4]:
-for obj in [s1, q1, q1_c1, q1_c2, q2, s2, q3, q3_c1, q3_c2, q3_c3, q3_c4, q3_c5, q4, q4_c1, q4_c2, q4_c3, fb_1, fb_2, fb_3, fb_4, a_f1_q1, a_f1_q2, a_f2_q1, a_f2_q2, a_f3_q3, a_f3_q4, a_f4_q3, a_f4_q4]:
+for obj in [s1, q1, q1_c1, q1_c2, q2, q3, q3_c1, q3_c2, q3_c3, q3_c4, q3_c5, q4, q4_c1, q4_c2, q4_c3, fb_1, fb_2, fb_3, fb_4, a_f1_q1, a_f1_q2, a_f2_q1, a_f2_q2, a_f3_q3, a_f3_q4, a_f4_q3, a_f4_q4]:
   session.add(obj)
   session.commit()
 
