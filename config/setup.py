60fc6d65566a4669a2cf17f4217b06417bd8d57d
PetNamedEr <skogberg.t.peter@student.uta.figit config --global user.emailgit config --global user.email skogberg.t.peter@student.uta.figit config --global user.email skogberg.t.peter@student.uta.fi>
2018-05-30 17:31:47 +0300

    Pythonanywhere.com setup version

--- config/setup.py
+++ config/setup.py
@@ -1,3 +1,6 @@
+'''
+This worked locally:
+
 import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
@@ -17,3 +20,25 @@ Base.metadata.bind = engine               # base class (+ my classes) bound to e
 DBSession = sessionmaker(bind=engine)     # session class bound to engine
 session = DBSession()                     # instance of session class
 
+
+...and this was needed to pythonanywhere.com:
+
+'''
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(
+    username="llb",
+    password="ILoveLLB2018",
+    hostname="llb.mysql.pythonanywhere-services.com",
+    databasename="llb$llb"
+)
+
+database_url = SQLALCHEMY_DATABASE_URI
+engine = create_engine(database_url)      # engine bound to type of database
+Base.metadata.bind = engine               # base class (+ my classes) bound to engine
+DBSession = sessionmaker(bind=engine)     # session class bound to engine
+session = DBSession()                     # instance of session class
